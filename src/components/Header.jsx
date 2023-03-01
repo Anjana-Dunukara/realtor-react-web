e993032a0610ff965af8ba1eac32e0aaa36a58ee
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-03-01 14:22:38 +0530

    Create routers to between pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Header() {
   const location = useLocation();
+  const navigate = useNavigate();
 
   const pathMathRoute = (route) => {
     if (route === location.pathname) {
@@ -11,35 +12,39 @@ export default function Header() {
   };
 
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 -z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
             src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
             alt="logo"
             className="h-5 cursor-pointer"
+            onClick={() => navigate("/")}
           />
         </div>
         <div>
           <ul className="flex space-x-10">
             <li
+              onClick={() => navigate("/")}
               className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRoute("/") && "text-black border-b-red-500"
-              }`}
+                pathMathRoute("/") && "text-gray-900 border-b-red-500"
+              } cursor-pointer`}
             >
               Home
             </li>
             <li
-              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRoute("/offers") && "text-black border-b-red-500"
+              onClick={() => navigate("/offers")}
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/offers") && "text-gray-900 border-b-red-500"
               }`}
             >
               Offers
             </li>
             <li
-              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRoute("/sign-in") && "text-black border-b-red-500"
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/sign-in") && "text-gray-900 border-b-red-500"
               }`}
+              onClick={() => navigate("/sign-in")}
             >
               Sign in
             </li>
