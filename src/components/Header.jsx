e6516c4da4d8a16f54f43fa7ea99aab47f096369
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-03-01 12:51:10 +0530

    Create and style navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+export default function Header() {
+  const location = useLocation();
+
+  const pathMathRoute = (route) => {
+    if (route === location.pathname) {
+      return true;
+    }
+  };
+
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 -z-50">
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <div>
+          <img
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="logo"
+            className="h-5 cursor-pointer"
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/") && "text-black border-b-red-500"
+              }`}
+            >
+              Home
+            </li>
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/offers") && "text-black border-b-red-500"
+              }`}
+            >
+              Offers
+            </li>
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/sign-in") && "text-black border-b-red-500"
+              }`}
+            >
+              Sign in
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+const path = require("path");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [path.join(__dirname, "./src/**/*.{js,jsx,ts,tsx}")],
   theme: {
     extend: {},
   },
